25d26559cbb58d678a174da6811eb4c8177640b7
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-06-21 12:51:11 -0300

    finished project

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import Button from './Button.css'
+import './Button.css'
 
 export default props => {
-
-    return(
-        <button className='button'>{props.label}</button>
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+    
+    return (
+        <button 
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>
     )
 }
